ba15c51d41c584bb0bd49e485ecb80af00973254
Niels Kersic <niels@zzapps.nl>
2024-11-16 13:17:03 +0100

    WIP implement the handler interface

--- gen/greet/v1/greetv1connect/greet.connect.go
+++ gen/greet/v1/greetv1connect/greet.connect.go
@@ -8,7 +8,7 @@ import (
 	connect "connectrpc.com/connect"
 	context "context"
 	errors "errors"
-	v1 "example/gen/greet/v1"
+	v1 "hello_connect/gen/greet/v1"
 	http "net/http"
 	strings "strings"
 )
